c8a0bf81aa0ed22fb726a2fff4752383741a2265
Jay Sheth <js_github@fastmail.fm>
2014-06-24 16:06:10 -0400

    fix indenting

--- run.php
+++ run.php
@@ -1,9 +1,6 @@
 <?php
-	//exit('exiting');
-	
-	error_reporting(E_ALL);
-	
-	require_once './vendor/autoload.php';
+
+    require_once './vendor/autoload.php';
 
     use Newbg\Main\BackgroundChanger;
 
